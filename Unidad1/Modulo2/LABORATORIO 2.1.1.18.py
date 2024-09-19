d289719a7dbc1e02b8df8551bb22452c622b605e
“Ruben-CT” <colmeneroby9@gmail.com>
2024-09-19 17:49:59 -0600

    Laboratorio 2.1.1.18

---
+++ Unidad1/Modulo2/LABORATORIO 2.1.1.18.py
@@ -0,0 +1,7 @@
+'''
+Nombre: Rubén COlmenro Sánchez
+Descripcion: Uso de la funcion print(), sep y and
+Fecha: 19/09/2024
+'''
+print("Fundamentos","Programación","en", sep="***", end="...")
+print("Python")

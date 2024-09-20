be9a3223edc7389cd1205e1454de9a77e52ef0ef
“Ruben-CT” <colmeneroby9@gmail.com>
2024-09-19 20:06:23 -0600

    Laboratorio 2.6.1.9

---
+++ Unidad1/Modulo2/LABORATORIO 2.6.1.9.py
@@ -0,0 +1,14 @@
+'''
+Nombre: Rubén COlmenro Sánchez
+Descripcion: Uso de la funcion print()
+Fecha: 19/09/2024
+'''
+x= float (input("ingresa un valor de x: "))#ingresa un valor flotante para la variable 
+y= float (input("ingresa un valor de y: "))#ingresa un valor flotante para la variable 
+
+print("Suma", x+y) # muestra el resultado de la suma aquí 
+print("Resta", x-y)# muestra el resultado de la resta aquí
+print("Multiplicacion" ,x*y)# muestra el resultado de la multiplicación aquí
+print("Division", x/y) # muestra el resultado de la división aquí
+
+print("\n¡Eso es todo, amigos!")

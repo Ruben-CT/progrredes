14e8c25b40a81bf5316e7c3cccd588bd5bc5d6f0
“Ruben-CT” <colmeneroby9@gmail.com>
2024-09-19 20:15:51 -0600

    Laboratorio 2.6.1.10

---
+++ Unidad1/Modulo2/  LABORATORIO2.6.1.10.py
@@ -0,0 +1,11 @@
+'''
+Nombre: Rubén COlmenro Sánchez
+Descripcion: Operadores y expreciones
+Fecha: 19/09/2024
+'''
+
+import math
+x = float(input("Ingresa el valor para x: "))
+
+y = 1 / (math.sqrt(x**2 + 1))
+print("y =", y)

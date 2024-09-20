d265b3ea117ef7223cbb565f9f8769f20c9e207e
“Ruben-CT” <colmeneroby9@gmail.com>
2024-09-19 19:19:10 -0600

    Laboratorio 2.4.1.7

---
+++ Unidad1/Modulo2/LABORATORIO 2.4.1.7.py
@@ -0,0 +1,12 @@
+'''
+Nombre: Rubén COlmenro Sánchez
+Descripcion: Uso de la funcion print(), sep y and
+Fecha: 19/09/2024
+'''
+juan = 3
+maria = 5
+adan = 6
+
+total_manzanas = juan + maria + adan
+
+print("Número Total de Manzanas:", total_manzanas)
